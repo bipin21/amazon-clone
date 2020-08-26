ccbf0bb96724b016ec552707f8c1dba38149beef
“Bipin <bipinkarki172@gmail.com>
2020-08-26 22:39:56 +0545

    Add products

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,61 @@
 import React from 'react'
 import './Home.css';
+import Product from './Product';
 
 function Home() {
     return (
         <div className="home">
             <img className="home__image" src="banner.png" />
+
+            {/* Product id, title, price, rating ,image */}
+            <div className="home__row">
+                <Product
+                    id={1}
+                    title="Title"
+                    price={11.96}
+                    rating={5}
+                    image="product1.png"
+                />
+                <Product
+                    id={1}
+                    title="Title 1"
+                    price={11.96}
+                    rating={5}
+                    image="product1.png"
+                />
+            </div>
+            <div className="home__row">
+                <Product
+                    id={1}
+                    title="Title"
+                    price={11.96}
+                    rating={5}
+                    image="product1.png"
+                />
+                <Product
+                    id={1}
+                    title="Title 1"
+                    price={11.96}
+                    rating={5}
+                    image="product1.png"
+                />
+                <Product
+                    id={1}
+                    title="Title 1"
+                    price={11.96}
+                    rating={5}
+                    image="product1.png"
+                />
+            </div>
+            <div className="home__row">
+                <Product
+                    id={1}
+                    title="Title"
+                    price={11.96}
+                    rating={5}
+                    image="product1.png"
+                />
+            </div>
         </div>
     )
 }
